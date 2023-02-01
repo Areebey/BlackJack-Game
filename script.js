e1086b2cf2179941b174c1f793fefaa534a2c322
Areebey <areebrafiq125@gmail.com>
2023-02-02 03:10:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Blackjack Game
+let firstCard =10;
+let secondCard = 5;
+let sum = firstCard+secondCard+6;
+
+let hasBlackJack=false
+let isAlive=true
+let message=""
+
+let messageEL=document.getElementById("message-el");
+
+let sumEl=document.querySelector("#sum-el");
+
+let cardEl=document.getElementById("card-el");
+
+function startGame(){
+if(sum <=20){
+    message="Do you want to draw a new caed";
+
+} else if (sum ===21){
+    message=("Yoo ! You are The Blackjack");
+    hasBlackJack=true
+}else {
+   message=("You are out of the game 😭");
+   isAlive=false
+}
+
+messageEL.textContent=message;
+cardEl.textContent="Cards: "+firstCard+" "+secondCard;
+sumEl.textContent="Sum: "+sum;
+}
+
+
